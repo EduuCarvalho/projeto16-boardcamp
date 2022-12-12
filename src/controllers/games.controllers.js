daf1bacc71d36db8c790bc06470fccf019c38103
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-12-12 00:01:42 -0300

    feat: logica na tora games buscar name  comquery

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,12 +1,28 @@
 import connection from "../database/db.js";
 
 export async function getGames(req,res){
-    const games = await connection.query('SELECT * FROM games;')
-    res.send(games.rows);
+    const {name} = req.query;
+
+    try{
+        if(name){
+            const gameQuery = await connection.query(
+                `SELECT * FROM games WHERE lower(name) LIKE $1;`,[name.toLowerCase()+"%"]
+            )
+            return res.send(gameQuery.rows)
+        }
+        const games = await connection.query('SELECT * FROM games;')
+        res.send(games.rows);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+
+
+   
 }
 
 export async function postGames (req,res){
     const {name , image, stockTotal ,categoryId, pricePerDay} = req.body;
+    try{
     if (name == null || name == 'undefined'){
         return res.sendStatus(400)
     }
@@ -23,4 +39,7 @@ export async function postGames (req,res){
     }
     const game = await connection.query(`INSERT INTO games ("name" , "image", "stockTotal" ,"categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5)`,[name , image, stockTotal ,categoryId, pricePerDay])
     res.send(game);
+}catch(err){
+    res.sendStatus(500);
+}
 }
